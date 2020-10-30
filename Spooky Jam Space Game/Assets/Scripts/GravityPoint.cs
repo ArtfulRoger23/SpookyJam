84aaad3600f9fd15787d0bb75f887b604dffd5f4
Sean <Sean_Balfour@Outlook.com>
2020-10-30 23:15:44 +0000

    Gravity

---
+++ Spooky Jam Space Game/Assets/Scripts/GravityPoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPoint : MonoBehaviour
+{
+    [SerializeField]
+    private float gravityScale;
+
+    [SerializeField]
+    private float planetRadius;
+
+    public float PlanetRadius { get => planetRadius; }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, planetRadius);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Vector3 dir = (transform.position - collision.transform.position) * gravityScale;
+
+        collision.GetComponent<Rigidbody2D>().AddForce(dir);
+
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.up = Vector3.MoveTowards(collision.transform.up, -dir, gravityScale * Time.deltaTime);
+        }
+    }
+}
---
+++ Spooky Jam Space Game/Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed = 10;
+    [SerializeField]
+    private float horizontal;
+    [SerializeField]
+    private float jumpPower;
+
+    [SerializeField]
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.right * horizontal * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Planet"))
+        {
+            rb.drag = 1f;
+
+            float distance = Mathf.Abs(collision.GetComponent<GravityPoint>().PlanetRadius - Vector2.Distance(transform.position, collision.transform.position));
+            Debug.Log(distance);
+
+            isGrounded = distance < 0.5f;
+
+            //if (distance < 0.5f)
+            //{
+            //    isGrounded = distance < 0.5f;
+            //}
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Planet"))
+        {
+            rb.drag = 0.2f;
+        }
+    }
+}
