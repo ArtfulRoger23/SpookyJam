99557a3c5f6f78cbfb47d1655e43eda74534da3a
Sean <Sean_Balfour@Outlook.com>
2020-10-31 22:23:49 +0000

    Pickup, Sounds and Planets

---
+++ Spooky Jam Space Game/Assets/Scripts/PickUp.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField]
+    private float rotateSpeed;
+    [SerializeField]
+    private string pickUpType;
+    [SerializeField]
+    private AudioSource pickUpSound;
+    [SerializeField]
+    private bool pickedUp;
+
+    public string PickUpType { get => pickUpType; }
+    public bool PickedUp { get => pickedUp; set => pickedUp = value; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickedUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 90, 0) * rotateSpeed * Time.deltaTime);
+
+        if (pickedUp && !pickUpSound.isPlaying)
+            Destroy(gameObject);
+    }
+
+    public void PickedUpItem()
+    {
+        if (pickedUp)
+            return;
+
+        pickedUp = true;
+
+        if (!pickUpSound.isPlaying)
+            pickUpSound.Play();
+    }
+}
